afca7a0091486ef5b6b35936d4c888e18e926fa4
Kuby (커비!) <thtjwls@gmail.com>
2024-11-10 23:35:16 +0000

    add build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,30 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import dts from "vite-plugin-dts";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-import { resolve } from "path"
+import { resolve } from "path";
+
+const entryFile = resolve(__dirname, "src/index.ts");
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: [],
+      tsconfigPath: "./tsconfig.app.json",
+    }),
+  ],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
     },
   },
   build: {
+    copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es', 'cjs']
-    }
-  }
-})
+      entry: entryFile,
+      formats: ["es", "cjs"],
+      fileName: (a) => `index${a !== "es" ? `.${a}` : ""}.js`,
+    },
+  },
+});
